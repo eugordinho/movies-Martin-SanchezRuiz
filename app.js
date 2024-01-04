c0cad16f1555a6ea195abc910542bb65f5549686
eugordinho <martinsanchezr@outlook.com>
2024-01-04 11:34:35 -0300

    changes

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const $article = document.getElementById('main-article')
 
-const moviesTittle = movies.tittle
+const createCard = (movie) => {
+    return `<article class="w-[200px]">
+    <img class="w-[200px]" src="${movie.image}" alt="Foto de parametro ${movie.title}">
+    <h2 class="font-semibold text-xl pt-3">${movie.title}</h2>
+    <p>${movie.overview}</p>
+    </article>`
+}
+
+movies.forEach(movie => $article.innerHTML += createCard(movie) )
