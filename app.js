16baf47c0c8d71d60916cd0738b848775e694a27
eugordinho <martinsanchezr@outlook.com>
2024-01-04 22:27:30 -0300

    Archivo completo

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const $article = document.getElementById('main-article')
 
 const createCard = (movie) => {
-    return `<article class="w-1/4">
-    <img class="w-100%" src="${movie.image}" alt="Foto de parametro ${movie.title}">
-    <h2 class="font-semibold text-xl pt-3">${movie.title}</h2>
+    return `<article class="max-w-sm h-98 m-2.5 border-solid border-4 rounded-lg p-2 bg-blue-500 text-black font-semibold ">
+    <img class="" src="${movie.image}" alt="Foto de parametro ${movie.title}">
+    <h2 class="font-semibold">${movie.title}</h2>
     <p>${movie.overview}</p>
     </article>`
 }
 
-movies.forEach(movie => $article.innerHTML += createCard(movie) )
+movies.forEach( movie => $article.innerHTML += createCard(movie) )
+
+
+
