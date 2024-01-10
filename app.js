881e4a562bbdc2db341e2394e077f181bb719461
eugordinho <martinsanchezr@outlook.com>
2024-01-10 01:29:31 -0300

    adding new features

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
+/* import { filterMoviebeByTitle, } from './modules/modules.js' */
+
 const $article = document.getElementById('main-article')
 
-const createCard = (movie) => {
-    return `<article class="max-w-sm h-98 m-2.5 border-solid border-4 rounded-lg p-2 bg-blue-500 text-black font-semibold ">
-    <img class="" src="${movie.image}" alt="Foto de parametro ${movie.title}">
-    <h2 class="font-semibold">${movie.title}</h2>
-    <p>${movie.overview}</p>
-    </article>`
+const $select = document.getElementById('select')
+
+const $search = document.querySelector( 'search' )
+
+const $movieSearch = document.getElementById( 'movieSearch' )
+
+const genresSet = new Set()
+
+const createCard = ( movie ) => {
+    return `<a href="./movies-details.html?id=${movie.id}"><article class="max-w-sm m-5 border-solid border-4 rounded-lg p-2 bg-blue-500 text-black font-semibold" style="height: 33rem;">
+    <img class="" src="${movie.image}" alt="Photo from ${movie.title}">
+    <h2 class="font-semibold mt-2.5">${movie.title}</h2>
+    <p class="mt-2.5">${movie.overview}</p>
+    </article></a>`
+}
+
+movies.forEach( movie => { 
+    for(genre of movie.genres) {
+        genresSet.add(genre)
+} } )
+
+genresSet.forEach( genre => $select.innerHTML += `<option value="${genre}">${genre}</option>` )
+
+function filterMoviebeByTitle( movies, inputTitle ) {
+    return movies.filter( movie => movie.title.toLowerCase().includes( inputTitle.value.toLowerCase() ) )
 }
 
 movies.forEach( movie => $article.innerHTML += createCard(movie) )
 
+$movieSearch.addEventListener( 'input', () => {
+    const filteredMovies = filterMoviebeByTitle( movies, $movieSearch )
+
+    $article.innerHTML = ''
+
+    filteredMovies.forEach( movie => $article.innerHTML += createCard(movie) )
+})
 
 
